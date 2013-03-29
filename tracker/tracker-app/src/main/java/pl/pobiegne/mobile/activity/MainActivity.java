979dc052e56352ce287954ca3bc0d1f3f61fdca1
Krzysztof <kopinski.krzysztof@gmail.com>
2013-03-29 19:04:31 +0100

    integracja z aa, formatka głowna

--- tracker/tracker-app/src/main/java/pl/pobiegne/mobile/activity/MainActivity.java
+++ tracker/tracker-app/src/main/java/pl/pobiegne/mobile/activity/MainActivity.java
@@ -2,13 +2,31 @@ package pl.pobiegne.mobile.activity;
 
 import pl.pobiegne.mobile.R;
 import android.app.Activity;
-import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+
+@EActivity(R.layout.main_layout)
 public class MainActivity extends Activity {
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-	}
+    
+    /**
+     * Nazwa klasy do logowania.
+     */
+    private final String TAG = this.getClass().getSimpleName();
+    
+    @ViewById(R.id.textView1)
+    protected TextView textView;
+    
+    
+    @AfterViews
+    void after() {
+        final String methodName = "after";
+        Log.d(TAG, "START - " + methodName);
 
+        Log.d(TAG, "END - " + methodName);
+    }
 }
