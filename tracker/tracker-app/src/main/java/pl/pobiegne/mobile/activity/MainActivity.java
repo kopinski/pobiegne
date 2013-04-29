67c7f383571648d84f48e263148330eb2c326e2a
Krzysztof <kopinski.krzysztof@gmail.com>
2013-04-29 23:24:27 +0200

    zapis trasy za pomocą xStream do pliku na karcie pamieci.

--- tracker/tracker-app/src/main/java/pl/pobiegne/mobile/activity/MainActivity.java
+++ tracker/tracker-app/src/main/java/pl/pobiegne/mobile/activity/MainActivity.java
@@ -1,24 +1,37 @@
 package pl.pobiegne.mobile.activity;
 
-import java.util.Locale;
+import java.util.ArrayList;
 
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
 import pl.pobiegne.mobile.R;
+import pl.pobiegne.mobile.adapter.BaseSpinnerAdapter;
+import pl.pobiegne.mobile.adapter.IconSpinnerAdapter;
 import pl.pobiegne.mobile.listener.TrackerListener;
+import pl.pobiegne.mobile.xml.converter.HashMapConverter;
+import pl.pobiegne.mobile.xml.converter.JodaTimeConverter;
+import pl.pobiegne.mobile.xml.data.GPX;
+import pl.pobiegne.mobile.xml.data.Track;
+import pl.pobiegne.mobile.xml.data.TrackSegment;
+import pl.pobiegne.mobile.xml.data.WayPoint;
 import android.app.Activity;
 import android.location.LocationManager;
 import android.util.Log;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.Bean;
 import com.googlecode.androidannotations.annotations.EActivity;
 import com.googlecode.androidannotations.annotations.SystemService;
+import com.googlecode.androidannotations.annotations.Trace;
 import com.googlecode.androidannotations.annotations.UiThread;
 import com.googlecode.androidannotations.annotations.ViewById;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.mapper.Mapper;
 
 
 @EActivity(R.layout.main_layout)
@@ -30,39 +43,85 @@ public class MainActivity extends Activity {
     private final String TAG = this.getClass().getSimpleName();
     
     @ViewById(R.id.gpsState)
-    protected TextView textView;
+    protected TextView gpsState;
     
-    @ViewById(R.id.workOutTime)
+    @ViewById(R.id.currentTime)
     protected TextView clock;
     
+    @ViewById(R.id.kind)
+    protected Spinner kindSpinner;
+    
+    @ViewById(R.id.activity)
+    protected Spinner activitySpinner;
+    
     @SystemService
-    LocationManager locationManager;
+    protected LocationManager locationManager;
     
     @Bean
-    TrackerListener trackerListener;
+    protected TrackerListener trackerListener;
+    
+    @Bean
+    protected BaseSpinnerAdapter adapter;
+    
+    @Bean
+    protected IconSpinnerAdapter iconAdapter;
+    
+    private DateTimeFormatter formatter;
     
     
     @AfterViews
     void after() {
         final String methodName = "after";
         Log.d(TAG, "START - " + methodName);
+        updateRequest();
+        formatter = DateTimeFormat.forPattern("HH:mm:ss");
+        clock.setText(formatter.print(DateTime.now()));
+        updateClock();
+        
+        activitySpinner.setAdapter(iconAdapter);
+        kindSpinner.setAdapter(adapter);
+        // XStream stream = new XStream();
+        // stream.autodetectAnnotations(true);
+        // Mapper mapper = stream.getMapper();
+        // ReflectionProvider reflectionProvider = stream.getReflectionProvider();
+        // WayPoint wayPoint = new WayPoint(new DateTime(), 12, 1.2, 23.2);
+        // TrackSegment trackSegment = new TrackSegment();
+        // trackSegment.getTrackPoints().add(wayPoint);trackSegment.getTrackPoints().add(wayPoint);trackSegment.getTrackPoints().add(wayPoint);trackSegment.getTrackPoints().add(wayPoint);trackSegment.getTrackPoints().add(wayPoint);trackSegment.getTrackPoints().add(wayPoint);
+        // Track track = new Track();
+        // track.getTrackSegments().add(trackSegment);
+        // ArrayList<Track> tracks = new ArrayList<Track>();
+        // tracks.add(track);
+        // GPX gpx = new GPX();
+        // gpx.setTracks(tracks);
+        // stream.registerConverter(new JodaTimeConverter());
+        // stream.registerConverter(new HashMapConverter(mapper, reflectionProvider, GPX.class, gpx.getHashMap()));
+        // Log.e(TAG, "XML - " + stream.toXML(gpx));
+        Log.d(TAG, "END - " + methodName);
+    }
+    
+    private void updateRequest() {
         if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
             locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, trackerListener);
         }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, trackerListener);
-        updateClock();
-        Log.d(TAG, "END - " + methodName);
     }
     
     @UiThread(delay = 1000)
     protected void updateClock() {
-        DateTimeFormatter formatter = DateTimeFormat.forPattern("hh:mm");
-        clock.setText(formatter.withLocale(Locale.getDefault()).print(DateTime.now()));
+        clock.setText(formatter.print(DateTime.now()));
+        updateClock();
     }
     
     @Override
     protected void onPause() {
         locationManager.removeUpdates(trackerListener);
+        super.onPause();
+    }
+    
+    @Override
+    @Trace
+    protected void onResume() {
+        updateRequest();
         super.onPause();
     }
 }
--- tracker/tracker-app/src/main/java/pl/pobiegne/mobile/listener/TrackerListener.java
+++ tracker/tracker-app/src/main/java/pl/pobiegne/mobile/listener/TrackerListener.java
@@ -1,21 +1,35 @@
 package pl.pobiegne.mobile.listener;
 
 import java.text.NumberFormat;
+import java.util.ArrayList;
 import java.util.Locale;
 
 import org.joda.time.DateTime;
 
 import pl.pobiegne.mobile.R;
 import pl.pobiegne.mobile.activity.MainActivity;
+import pl.pobiegne.mobile.util.StorageUtil;
+import pl.pobiegne.mobile.xml.converter.HashMapConverter;
+import pl.pobiegne.mobile.xml.converter.JodaTimeConverter;
+import pl.pobiegne.mobile.xml.data.GPX;
+import pl.pobiegne.mobile.xml.data.Track;
+import pl.pobiegne.mobile.xml.data.TrackSegment;
+import pl.pobiegne.mobile.xml.data.WayPoint;
 import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationProvider;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EBean;
 import com.googlecode.androidannotations.annotations.RootContext;
+import com.googlecode.androidannotations.annotations.Trace;
 import com.googlecode.androidannotations.annotations.ViewById;
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.reflection.ReflectionProvider;
+import com.thoughtworks.xstream.mapper.Mapper;
 
 
 @EBean
@@ -38,7 +52,7 @@ public class TrackerListener implements LocationListener {
     MainActivity context;
     
     @ViewById(R.id.gpsState)
-    protected TextView textView;
+    protected TextView gpsState;
     
     @ViewById(R.id.distance)
     protected TextView distance;
@@ -46,26 +60,57 @@ public class TrackerListener implements LocationListener {
     @ViewById(R.id.altitude)
     protected TextView altitude;
     
-    @ViewById(R.id.currentTime)
-    protected TextView time;
+    private StringBuffer stringBuffer;
     
+    private GPX gpx = new GPX();
+    
+    private WayPoint wayPoint;
+    
+    private XStream stream;
+    
+    private TrackSegment trackSegment;
+    
+    @Trace
+    @Click(R.id.start)
+    protected void click() {
+        StorageUtil storage = new StorageUtil(context);
+        storage.saveStringToExternalStorage(stream.toXML(gpx), "pobiegne", new DateTime().toString("HHmm")+".xml", true);
+    }
     
     public TrackerListener() {
         numberFormat = NumberFormat.getInstance(Locale.getDefault());
         numberFormat.setMaximumFractionDigits(3);
         numberFormat.setMinimumFractionDigits(3);
         totalDistance = 0.0;
+        stringBuffer = new StringBuffer();
+        stream = new XStream();
+        stream.autodetectAnnotations(true);
+        Mapper mapper = stream.getMapper();
+        ReflectionProvider reflectionProvider = stream.getReflectionProvider();
+        stream.registerConverter(new JodaTimeConverter());
+        stream.registerConverter(new HashMapConverter(mapper, reflectionProvider, GPX.class, gpx.getHashMap()));
     }
     
     @Override
     public void onLocationChanged(final Location location) {
         // final String methodName = "onLocationChanged";
         // Log.d(TAG, "START - " + methodName);
-        // textView.setText("CHA");
-        Log.d(TAG,
-                "lat: " + location.getLatitude() + ", lon: " + location.getLongitude() + ", fix: "
-                        + location.getAccuracy() + ", time: "
-                        + new DateTime(location.getTime()).toLocalTime().toString());
+        gpsState.setText(context.getResources().getText(R.string.ok));
+        stringBuffer.setLength(0);
+        stringBuffer.append("lat: ");
+        stringBuffer.append(location.getLatitude());
+        stringBuffer.append(", lon: ");
+        stringBuffer.append(location.getLongitude());
+        stringBuffer.append(", fix: ");
+        stringBuffer.append(location.getAccuracy());
+        wayPoint =
+                new WayPoint(location.getAltitude(), new DateTime(), location.getLongitude(), location.getLatitude());
+        
+        if (trackSegment == null) {
+            trackSegment = new TrackSegment();
+        }
+        trackSegment.getTrackPoints().add(wayPoint);
+        Log.d(TAG, stringBuffer.toString());
         if (firstDateTime == null) {
             firstDateTime = new DateTime(location.getTime());
         }
@@ -73,7 +118,6 @@ public class TrackerListener implements LocationListener {
             totalDistance += oldLocation.distanceTo(location) / 1000;
             distance.setText(numberFormat.format(totalDistance));
             altitude.setText(String.valueOf(location.getAltitude()));
-            time.setText(new DateTime().toString("HH:mm:ss", Locale.getDefault()));
         }
         oldLocation = location;
         // Log.d(TAG, "END - " + methodName);
@@ -83,7 +127,7 @@ public class TrackerListener implements LocationListener {
     public void onProviderDisabled(final String provider) {
         final String methodName = "onProviderDisabled";
         Log.d(TAG, "START - " + methodName);
-        textView.setText("DIS");
+        gpsState.setText(R.string.not_available);
         Log.d(TAG, "END - " + methodName);
     }
     
@@ -91,7 +135,7 @@ public class TrackerListener implements LocationListener {
     public void onProviderEnabled(final String provider) {
         final String methodName = "onProviderEnabled";
         Log.d(TAG, "START - " + methodName);
-        textView.setText("EN");
+        gpsState.setText(R.string.ok);
         Log.d(TAG, "END - " + methodName);
     }
     
@@ -99,6 +143,32 @@ public class TrackerListener implements LocationListener {
     public void onStatusChanged(final String provider, final int status, final Bundle extras) {
         final String methodName = "onStatusChanged";
         Log.d(TAG, "START - " + methodName);
+        switch (status) {
+            case LocationProvider.OUT_OF_SERVICE:
+                gpsState.setText(R.string.not_available);
+                break;
+            
+            case LocationProvider.TEMPORARILY_UNAVAILABLE:
+                gpsState.setText(R.string.not_available);
+                break;
+            
+            case LocationProvider.AVAILABLE:
+                if (trackSegment == null) {
+                    trackSegment = new TrackSegment();
+                }
+                Track track = new Track();
+                track.setName("Trasa z dnia" + new DateTime().toString("dd-MM-yyyy HH:mm"));
+                track.getTrackSegments().add(trackSegment);
+                ArrayList<Track> tracks = new ArrayList<Track>();
+                tracks.add(track);
+                gpx.setTracks(tracks);
+                gpsState.setText(R.string.ok);
+                break;
+            
+            default:
+                gpsState.setText(R.string.startValue);
+                break;
+        }
         Log.d(TAG, "provider: " + provider + ", status: " + status);
         Log.d(TAG, "END - " + methodName);
     }
